982976cd5da9b101240ed03c3cfb101f4722de7e
Alessandro Riva <alessandro.riva@elmec.it>
2026-04-20 17:08:34 +0200

    rebrand

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chinese-travel-v7';
+const CACHE = 'handy-v1';
 const ASSETS = ['./', './index.html', './content.json', './style.css', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {
