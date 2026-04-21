8608e4815f3d84af1997ed1871e5d74f09219143
Alessandro Riva <alex27riva@gmail.com>
2026-04-21 21:54:00 +0200

    feat: add visa free countries

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'handy-v8';
+const CACHE = 'handy-v9';
 const ASSETS = ['./', './index.html', './content.json', './style.css', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {
